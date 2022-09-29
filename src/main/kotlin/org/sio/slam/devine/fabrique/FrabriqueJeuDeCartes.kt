c20c0a84df17fb6dfc9b4140d6aecbc759b0bd09
Nenuphare <caye.damien@gmail.com>
2022-09-29 15:47:02 +0200

    Implémentation de createJeu32Cartes()

--- src/main/kotlin/org/sio/slam/devine/fabrique/FrabriqueJeuDeCartes.kt
+++ src/main/kotlin/org/sio/slam/devine/fabrique/FrabriqueJeuDeCartes.kt
@@ -10,8 +10,14 @@ import org.sio.slam.devine.enum.NomCarte
  */
 fun createJeu32Cartes() : List<Carte> {
     return listOf(
-        Carte(NomCarte.VALET, CouleurCarte.COEUR),
-        Carte(NomCarte.AS, CouleurCarte.PIQUE),
+        Carte(NomCarte.DIX, CouleurCarte.PIQUE),
+        Carte(NomCarte.NEUF, CouleurCarte.PIQUE),
+        Carte(NomCarte.HUIT,CouleurCarte.PIQUE),
+        Carte(NomCarte.SEPT, CouleurCarte.PIQUE),
+        Carte(NomCarte.VALET, CouleurCarte.PIQUE),
+        Carte(NomCarte.DAME, CouleurCarte.PIQUE),
+        Carte(NomCarte.ROI,CouleurCarte.PIQUE),
+        Carte(NomCarte.AS,CouleurCarte.PIQUE),
         Carte(NomCarte.DIX, CouleurCarte.CARREAU),
         Carte(NomCarte.NEUF, CouleurCarte.CARREAU),
         Carte(NomCarte.HUIT,CouleurCarte.CARREAU),
@@ -20,9 +26,24 @@ fun createJeu32Cartes() : List<Carte> {
         Carte(NomCarte.DAME, CouleurCarte.CARREAU),
         Carte(NomCarte.ROI,CouleurCarte.CARREAU),
         Carte(NomCarte.AS,CouleurCarte.CARREAU),
+        Carte(NomCarte.DIX, CouleurCarte.COEUR),
+        Carte(NomCarte.NEUF, CouleurCarte.COEUR),
+        Carte(NomCarte.HUIT,CouleurCarte.COEUR),
+        Carte(NomCarte.SEPT, CouleurCarte.COEUR),
+        Carte(NomCarte.VALET, CouleurCarte.COEUR),
+        Carte(NomCarte.DAME, CouleurCarte.COEUR),
+        Carte(NomCarte.ROI,CouleurCarte.COEUR),
+        Carte(NomCarte.AS,CouleurCarte.COEUR),
+        Carte(NomCarte.DIX, CouleurCarte.TREFLE),
+        Carte(NomCarte.NEUF, CouleurCarte.TREFLE),
+        Carte(NomCarte.HUIT,CouleurCarte.TREFLE),
+        Carte(NomCarte.SEPT, CouleurCarte.TREFLE),
+        Carte(NomCarte.VALET, CouleurCarte.TREFLE),
+        Carte(NomCarte.DAME, CouleurCarte.TREFLE),
+        Carte(NomCarte.ROI,CouleurCarte.TREFLE),
+        Carte(NomCarte.AS,CouleurCarte.TREFLE),
 
     )
 }
 
-// TODO Création d'un paquet de 52 cartes à implémenter (de 2 à AS)
 
