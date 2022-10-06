aabf3295092d525c67c220e568fc8c218a8e57b1
benji <benji.ngt@gmail.com>
2022-10-06 14:23:38 +0200

    carte a deviner

--- src/main/kotlin/org/sio/slam/devine/MainPlayConsole.kt
+++ src/main/kotlin/org/sio/slam/devine/MainPlayConsole.kt
@@ -8,18 +8,34 @@ import org.sio.slam.devine.enum.NomCarte
 import org.sio.slam.devine.enum.getCouleurCarteFromString
 import org.sio.slam.devine.enum.getNomCarteFromString
 import org.sio.slam.devine.fabrique.createJeu32Cartes
+import org.sio.slam.devine.fabrique.createJeu52Cartes
 
 fun main(args: Array<String>) {
 
-    val aide = false
     // TODO (A) demander au joueur s'il souhaite avoir de l'aide pour sa partie
+    val aide: Boolean
+    println("Souhaitez-vous avoir de l'aide durant la partie ? oui ou non")
+    val wantAide : String = readLine() + ""
+    aide = wantAide == "oui"
 
-    println("Création d'un paquet de 32 cartes")
-    val paqueDeCartes = Paquet(createJeu32Cartes())
+
     // TODO (A) demander au joueur avec quel jeu de cartes 32 ou 52 il souhaite jouer
+    val paquetDeCartes: Paquet
+    println("Souhaitez-vous avoir un paquet de 32 ou 52 cartes ?")
+    val choixPaquet : String = readLine() + ""
+
+    if (choixPaquet == "32") {
+        println("Création d'un paquet de 32 cartes")
+        paquetDeCartes = Paquet(createJeu32Cartes())
+    }
+    else {
+        println("Création d'un paquet de 52 cartes")
+        paquetDeCartes = Paquet(createJeu52Cartes())
+    }
+
 
     println(" ==== Instanciation du jeu, début de la partie. ====")
-    val jeu = Jeu(aide, paqueDeCartes)
+    val jeu = Jeu(aide, paquetDeCartes)
 
     println("Entrez un nom de carte dans le jeu (exemples : Roi, sept, six, As...) :")
     // TODO (optionnel) permettre de saisir un chiffre au lieu d'une chaine : 7 au lieu de Sept...
@@ -30,9 +46,12 @@ fun main(args: Array<String>) {
     val couleurCarteUserStr: String = readLine() + ""
     val couleurCarteUser: CouleurCarte? = getCouleurCarteFromString(couleurCarteUserStr.trim().uppercase())
 
+
     if (nomCarteUser != null && couleurCarteUser != null) {
         // prise en compte de la carte du joueur
         val carteDuJoueur: Carte = Carte(nomCarteUser, couleurCarteUser)
+
+        //val carteADeviner: Carte = Carte()
 
         if (jeu.isMatch(carteDuJoueur)) {
             println("Bravo, vous avez trouvé la bonne carte !")
@@ -53,13 +72,13 @@ fun main(args: Array<String>) {
     }
 
     // TODO (A) permettre au joueur de retenter une autre carte (sans relancer le jeu) ou d'abandonner la partie
-    println(" ==== Fin prématurée de la partie ====")
+    //println(" ==== Fin prématurée de la partie ====")
 
     // TODO (A) Présenter à la fin la carte à deviner
-    println("TODO Présenter ici la carte à deviner")
+    println("La carte à deviner était le ${jeu.carteADeviner}")
 
     // TODO (challenge-4) la stratégie de jeu est à implémenter... à faire lorsque les autres TODOs auront été réalisés
-    println("Votre stratégie de jeu : ${jeu.strategiePartie()} ")
+    //println("Votre stratégie de jeu : ${jeu.strategiePartie()} ")
 
     println(" ==== Fin de la partie. ====")
 }
